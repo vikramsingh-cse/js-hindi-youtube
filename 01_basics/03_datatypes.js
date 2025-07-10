1ab22d4fc1b625471a54066775f28870da2dd877
vikramsingh-cse <vikrams83048@gmail.com>
2025-07-10 06:26:22 +0000

    datatypesdone

---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";  //treat all JS code as newer version
+ 
+//alert (3+3) we are using nodejs,not broweser
+
+console.log(3+
+    3
+);  //code readability should be high
+
+console.log("VIKRAM");
+
+let name = "vikram"
+let age = 19
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint => for large number (use trading)
+//string => ""
+//boolean => true/false
+//null =>standalone value
+//undefined =>
+//symbol =>unique
+//object =>
+
+
+    console.log(typeof "vikram");
+    console.log(typeof null);  //object
+    console.log(typeof undefined);  //undefined
+    
+    
