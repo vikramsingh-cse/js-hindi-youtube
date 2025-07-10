6bf84a3130a6ed6f8cde72616cf673039d734214
vikramsingh-cse <vikrams83048@gmail.com>
2025-07-10 06:26:02 +0000

    variablesdone

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail ="vikram@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2  not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "Bangaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword,accountCity])
